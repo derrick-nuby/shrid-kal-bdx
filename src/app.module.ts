9de9b205bbabf4e20789c22b4c8b3329d1792fe0
Derrick-Nuby <iradukundaderrick7@gmail.com>
2025-02-07 17:32:22 +0200

    imported task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TaskModule,
   ],
   controllers: [AppController],
   providers: [AppService],
