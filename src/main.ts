25ec17b8a50ffe4ccf8ce9f9de22423d1efd154c
Derrick-Nuby <iradukundaderrick7@gmail.com>
2025-02-07 17:54:03 +0200

    api versioning done

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
   const appName = process.env.APP_NAME ?? 'App';
-  await app.listen(port);
+  const apiPrefix = 'api';
 
+  app.setGlobalPrefix(apiPrefix, {
+    exclude: ['/', 'api/v1']
+  });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+
+  await app.listen(port);
   Logger.log(`${appName} has started successfully and is running on port ${port} and you can run it on http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
